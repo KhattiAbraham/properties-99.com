4beeb11ad3bd8b9bad69003261cc52cb04ff2b5e
KhattiAbraham <abrahamkhatti@gmail.com>
2025-10-08 18:17:07 +0530

    Second Commit

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ let mongoose = require("mongoose")
 let app = express()
 let ownerModel = require('./models/ownerModel')
 let customerModel = require('./models/customerModel')
-const { error } = require('console')
+const propertyModel = require('./models/propertyModel')
 app.use(express.json())
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}))
-require("dotenv").config()
 
 
-mongoose.connect(process.env.MONGO_URI, {
+const uri = "mongodb+srv://abrahamkhatti:k123y@cluster0.wqncw6x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -27,24 +28,76 @@ app.get("/owner-login", (req, res)=>{
     res.render('owner-login')
 })
 
-app.get("/customer-login", (req, res)=>{
-    res.render('customer-login')
+app.post("/owner-login", async (req, res) => {
+  try {
+    const { username, phone } = req.body;
+    const user = await ownerModel.findOne({ phone });
+    if(user)
+    {
+      res.redirect(`/create-listings/${user._id}`)
+    }
+    else{
+      res.status(500).send("User Not Found")
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/create-listings/:id", async(req,res)=>
+{
+  let id = req.params.id;
+  res.render('create-property', {id})
+}
+   
+)
+
+app.post("/create-listings/:id", async (req, res) => {
+  try {
+    const { title, location, price } = req.body;
+    const owner = req.params.id
+    const user = await propertyModel.create({ 
+      title,
+      location,
+      price,
+      owner});
+    if(user)
+    {
+      res.redirect(`/create-listings/${owner}`)
+    }
+    else{
+      console.log("User Not Created")
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/my-listings/:id", async(req,res)=>{
+  let allListings = await propertyModel.find({ owner: req.params.id})
+  console.log(allListings)
+  res.render("properties", {allListings})
 })
 
 app.post("/customer-login", async (req, res) => {
   try {
     const { username, phone } = req.body;
-    const newUser = await customerModel.findOne({ phone });
-    if(newUser)
+    const user = await customerModel.findOne({ phone });
+    if(user)
     {
-        console.log(newUser)
-        res.redirect("/available-properties")        
-    }}
-    catch (error) {
-        console.log(error)
+      res.redirect("/available-properties")
     }
+    else{
+      res.status(500).send("User Not Found")
+    }
+  } catch (err) {
+    console.error(err);
+  }
 });
 
+app.get("/customer-login", (req, res)=>{
+    res.render('customer-login')
+})
 
 
 app.get("/owner-signup", (req, res)=>{
--- models/propertyModel.js
+++ models/propertyModel.js
@@ -1,7 +1,10 @@
+let mongoose = require('mongoose')
+
 let propertyModel = new mongoose.Schema({
     title: String,
     location: String,
-    description: String,
+    price: String,
+   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'owner' }
 })
 
 module.exports = mongoose.model("property", propertyModel)
