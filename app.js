45e37c47cff897f73772374ab98690520fc2b5a9
KhattiAbraham <156577900+KhattiAbraham@users.noreply.github.com>
2025-10-10 10:46:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}))
-require.dotenv('config')
+require('dotenv').config()
 
 mongoose.connect(process.env.MONG_URI, {
   useNewUrlParser: true,
